856783564b3fdb4719ad4027b643fa6c66f71efb
John Henderson <john.henderson728@gmail.com>
2023-01-24 11:41:47 +1100

    Cleaned up generate RTS

--- generate_RTS_data.py
+++ generate_RTS_data.py
@@ -2,47 +2,50 @@ import numpy as np
 import pandas as pd
 from generate_RTS import generate_RTS, generate_gaussian_noise
 import sys
+import pickle 
 
-data = {'rts': [], 'noisy_signal': []}
+def generate_data(num_data_points, num_samples=1000, noise=False, 
+                  verbose=True, file_name='signals.pkl',
+                  num_states=2, transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]])):
 
-num_data = 10
-num_samples = 5
+    data = {'rts': [], 'noisy_signal': []}
 
-# print a pretty "processing" sign
-with open("ascii_text.txt", "r") as file:
-    for line in file:
-        print(line[:-1])
+    if not noise:
+        noise = generate_gaussian_noise(num_samples=num_samples, mean=0, std=0.5)
 
-for i in range(num_data):
-    # Generate RTS signal
-    rts = generate_RTS(
-        num_states=2, 
-        transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]]), 
-        num_samples=num_samples
-    )
+    # print a pretty "processing" sign
+    if verbose:
+        with open("ascii_text.txt", "r") as file:
+            for line in file:
+                print(line[:-1])
 
-    # Generate Gaussian noise
-    noise = generate_gaussian_noise(num_samples=num_samples, mean=0, std=0.5)
-    
-    # Add the noise to the RTS signal
-    noisy_signal = rts + noise
+    for i in range(num_data_points):
 
-    data['rts'].append(rts)
-    # data['gaussian_noise'].append(noise)
-    data['noisy_signal'].append(noisy_signal)
+        # Generate RTS signal
+        rts = generate_RTS(
+            num_states=num_states, 
+            transition_probs=transition_probs, 
+            num_samples=num_samples
+        )
 
-    # print a pretty percentage
-    percentage = round(((i + 1)/num_data) * 100, 3)
-    sys.stdout.write("\r{:.2f}% completed".format(percentage))
-    sys.stdout.flush()
+        # Add the noise to the RTS signal
+        noisy_signal = rts + noise
 
-print()
+        data['rts'].append(rts)
+        # data['gaussian_noise'].append(noise)
+        data['noisy_signal'].append(noisy_signal)
 
-print("Exporting data to file...")
+        # print a pretty percentage
+        if verbose:
+            percentage = round(((i + 1)/num_data_points) * 100, 3)
+            sys.stdout.write("\r{:.2f}% completed".format(percentage))
+            sys.stdout.flush()
 
-# Create a pandas dataframe
-df = pd.DataFrame(data)
+    if verbose:
+        print("Exporting data to file...")
+    with open(file_name, 'wb') as f:
+        pickle.dump(data, f)
 
-# Export the dataframe to a CSV file
-df.to_csv('signals.csv', index=False)
 
+if __name__ == '__main__':
+    generate_data(10, 5, verbose=True, file_name='signals.pkl')
