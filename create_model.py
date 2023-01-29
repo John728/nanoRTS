42d1961604e1d509f989a5babb96710f410dcfe9
John Henderson <john.henderson728@gmail.com>
2023-01-29 16:47:30 +1100

    Written generate_graphs.py

--- create_model.py
+++ create_model.py
@@ -4,13 +4,15 @@ import sys
 import numpy as np
 import pandas as pd
 from IPython.display import display
+import tensorflow as tf
 from keras import layers
-from keras import Sequential
+from keras import Sequential, Model
 from keras.callbacks import EarlyStopping, Callback, ReduceLROnPlateau
 import pickle
 from ast import literal_eval
-from keras.layers import Input, Conv1D, MaxPooling1D, UpSampling1D, Dropout
+from keras.layers import Input, Conv1D, MaxPooling1D, UpSampling1D, Dropout, Dense, Flatten, Reshape
 from keras.models import Model
+import time
 
 class Print_Progress(Callback):
     def on_train_begin(self, logs={}):
@@ -28,58 +30,65 @@ class Print_Progress(Callback):
         print()
         print("done!")
 
-# Load the data from each file in ./data
-data = {'rts': [], 'noisy_signal': []}
-for file in os.listdir('./data'):
-    sys.stdout.write("\rLoading file: " + file + "...")
-    with open('./data/' + file, 'rb') as f:
-        data_tmp = pickle.load(f)
-        data.update(data_tmp)
-    f.close()
+def load_and_preprocess_data(filename):
+    data = {'rts': [], 'noisy_signal': []}
+    for filename in os.listdir('./data'):
+        if filename.endswith('.tfrecord'):
+            record_iterator = tf.compat.v1.io.tf_record_iterator(path=os.path.join('./data', filename))
+            for string_record in record_iterator:
+                example = tf.train.Example()
+                example.ParseFromString(string_record)
+                rts = example.features.feature['rts'].float_list.value
+                noisy_signal = example.features.feature['noisy_signal'].float_list.value
+                data['rts'].append(rts)
+                data['noisy_signal'].append(noisy_signal)
 
 
-df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
-print("\nData loaded")
+    print("\nData loaded")
 
-# find out the number of samples in each signal
-num_samples = len(df['rts'][0])
+    # find out the number of samples in each signal
+    num_samples = len(df['rts'][0])
 
-# Create training and validation splits
-df_train = df.sample(frac=0.7, random_state=0)
-df_valid = df.drop(df_train.index)
+    # Create training and validation splits
+    df_train = df.sample(frac=0.7, random_state=0)
+    df_valid = df.drop(df_train.index)
 
-# Split the data into features and target
-X_train = np.array(df_train['noisy_signal'].tolist())
-y_train = np.array(df_train['noisy_signal'].tolist())
-X_valid = np.array(df_valid['rts'].tolist())
-y_valid = np.array(df_valid['rts'].tolist())
+    # Split the data into features and target
+    X_train = np.array(df_train['noisy_signal'].tolist())
+    y_train = np.array(df_train['noisy_signal'].tolist())
+    X_valid = np.array(df_valid['rts'].tolist())
+    y_valid = np.array(df_valid['rts'].tolist())
 
-def generate_autoencoder(input_shape, file_name, epochs=1000, batch_size=32, verbose=0,
-                         num_samples=1000, callbacks=[Print_Progress(), EarlyStopping(patience=50, restore_best_weights=True)]):
+    return X_train, y_train, X_valid, y_valid, num_samples
+
+# def generate_random_forrest(input_shape, file_name, epochs=1000, batch_size=32, verbose=0,
+#                             num_samples=1000, callbacks=[Print_Progress(), EarlyStopping(patience=50, restore_best_weights=True)]):
+#     # define random forrest
+#     model = RandomForestRegressor(n_estimators=1000, random_state=0, n_jobs=-1)
+
+
+def generate_general_nn(X_train, y_train, X_valid, y_valid, input_shape, file_name, epochs=1000, batch_size=32, verbose=0):
+    
+    start_time = time.time()
+
     # define the model
     input_layer = Input(shape=input_shape)
 
-    encoded = Conv1D(16, 3, activation='relu', padding='same')(input_layer)
-    encoded = MaxPooling1D(2, padding='same')(encoded)
-    encoded = Dropout(0.5)(encoded)
-    encoded = Conv1D(8, 3, activation='relu', padding='same')(encoded)
-    encoded = MaxPooling1D(2, padding='same')(encoded)
-    encoded = Dropout(0.5)(encoded)
-
-    decoded = Conv1D(8, 3, activation='relu', padding='same')(encoded)
-    decoded = UpSampling1D(2)(decoded)
-    encoded = Dropout(0.5)(decoded)
-    decoded = Conv1D(16, 3, activation='relu', padding='same')(decoded)
-    decoded = UpSampling1D(2)(decoded)
-    encoded = Dropout(0.5)(decoded)
-    decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(decoded)
+    layer = Dense(2000, activation='relu')(input_layer)
+    layer = Dense(3000, activation='relu')(layer)
+    layer = Dense(2000, activation='relu')(layer)
+    layer = Dense(1000, activation='relu')(layer)
+    layer = Dense(1000, activation='relu')(layer)
+    
 
     # create the autoencoder model
     autoencoder = Model(input_layer, decoded)
     autoencoder.compile(optimizer='adam', loss='mean_squared_error')
 
-    print("Pre-processing complete")
+    if verbose:
+        print("Pre-processing complete")
 
     # # Create the early stopping callback
     early_stopping = EarlyStopping(
@@ -98,18 +107,31 @@ def generate_autoencoder(input_shape, file_name, epochs=1000, batch_size=32, ver
     print_progress = Print_Progress()
     print_progress.set_model(autoencoder)
 
-    print("Fitting model...")
+    if verbose:
+        print("Fitting model...")
 
     # # Fit the model
-    history = autoencoder.fit(
-        X_train, y_train,
-        validation_data=(X_valid, y_valid),
-        batch_size=200,
-        epochs=1000,
-        callbacks=[early_stopping, print_progress, reduce_lr],
-        verbose=0,
-        initial_epoch=0
-    )
+
+    if verbose:
+        history = autoencoder.fit(
+            X_train, y_train,
+            validation_data=(X_valid, y_valid),
+            batch_size=batch_size,
+            epochs=epochs,
+            callbacks=[early_stopping, print_progress, reduce_lr],
+            verbose=1,
+            initial_epoch=0
+        )
+    else:
+        history = autoencoder.fit(
+            X_train, y_train,
+            validation_data=(X_valid, y_valid),
+            batch_size=batch_size,
+            epochs=epochs,
+            callbacks=[early_stopping, reduce_lr],
+            verbose=0,
+            initial_epoch=0
+        )
 
     # save the history
     with open(file_name + '_history', 'wb') as f:
@@ -117,6 +139,101 @@ def generate_autoencoder(input_shape, file_name, epochs=1000, batch_size=32, ver
     f.close()
     
     autoencoder.save(file_name)
+    end_time = time.time()
+
+    return end_time - start_time
+
+def generate_autoencoder(X_train, y_train, X_valid, y_valid, input_shape, file_name, 
+                         epochs=1000, batch_size=32, verbose=0):
+    
+    start_time = time.time()
+
+    # define the model
+    input_layer = Input(shape=input_shape)
+
+    encoded = Conv1D(32, 3, activation='relu', padding='same')(input_layer)
+    encoded = MaxPooling1D(2, padding='same')(encoded)
+    encoded = Dropout(0.5)(encoded)
+    encoded = Conv1D(64, 3, activation='relu', padding='same')(encoded)
+    encoded = MaxPooling1D(2, padding='same')(encoded)
+    encoded = Dropout(0.5)(encoded)
+    encoded = Conv1D(128, 3, activation='relu', padding='same')(encoded)
+    encoded = MaxPooling1D(2, padding='same')(encoded)
+    encoded = Dropout(0.5)(encoded)
+
+    decoded = Conv1D(128, 3, activation='relu', padding='same')(encoded)
+    decoded = UpSampling1D(2)(decoded)
+    decoded = Dropout(0.5)(decoded)
+    decoded = Conv1D(64, 3, activation='relu', padding='same')(decoded)
+    decoded = UpSampling1D(2)(decoded)
+    decoded = Dropout(0.5)(decoded)
+    decoded = Conv1D(32, 3, activation='relu', padding='same')(decoded)
+    decoded = UpSampling1D(2)(decoded)
+    decoded = Dropout(0.5)(decoded)
+    decoded = Conv1D(1, 3, activation='sigmoid', padding='same')(decoded)
+
+    # create the autoencoder model
+    autoencoder = Model(input_layer, decoded)
+    autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+
+    if verbose:
+        print("Pre-processing complete")
+
+    # # Create the early stopping callback
+    early_stopping = EarlyStopping(
+        min_delta=0.001, # minimium amount of change to count as an improvement
+        patience=50, # how many epochs to wait before stopping
+        restore_best_weights=True,
+    )
+
+    reduce_lr = ReduceLROnPlateau(
+        monitor='loss',
+        factor=0.5,
+        patience=10,
+        min_lr=0.0001
+    )
+
+    print_progress = Print_Progress()
+    print_progress.set_model(autoencoder)
+
+    if verbose:
+        print("Fitting model...")
+
+    # # Fit the model
+
+    if verbose:
+        history = autoencoder.fit(
+            X_train, y_train,
+            validation_data=(X_valid, y_valid),
+            batch_size=batch_size,
+            epochs=epochs,
+            callbacks=[early_stopping, print_progress, reduce_lr],
+            verbose=1,
+            initial_epoch=0
+        )
+    else:
+        history = autoencoder.fit(
+            X_train, y_train,
+            validation_data=(X_valid, y_valid),
+            batch_size=batch_size,
+            epochs=epochs,
+            callbacks=[early_stopping, reduce_lr],
+            verbose=0,
+            initial_epoch=0
+        )
+
+    # save the history
+    with open(file_name + '_history', 'wb') as f:
+        pickle.dump(history.history, f)
+    f.close()
+    
+    autoencoder.save(file_name)
+    end_time = time.time()
+
+    return end_time - start_time
 
 if __name__ == "__main__":
-    generate_autoencoder((num_samples, 1), './autoencoder_model')
+
+    x_train, y_train, x_valid, y_valid, num_samples = load_and_preprocess_data('./data/')
+    generate_autoencoder(x_train, y_train, x_valid, y_valid, (num_samples, 1), './autoencoder_model', verbose=1)
+    # generate_general_nn(x_train, y_train, x_valid, y_valid, (num_samples, 1), './general_nn_model')
--- generate_RTS_data.py
+++ generate_RTS_data.py
@@ -1,17 +1,21 @@
+import tensorflow as tf
 import numpy as np
 import pandas as pd
 from generate_RTS import generate_RTS, generate_gaussian_noise, generate_sinusoidal_signal
 import sys
 import pickle 
-import shelve
+import os
+import time
 
 def generate_data(num_data_points=10000, num_samples=1000, 
-                  noise=generate_gaussian_noise(num_samples=1000, mean=0, std=0.5),
+                #   noise=generate_gaussian_noise(num_samples=1000, mean=0, std=0.5),
                   vary_noise=False,
                   verbose=True, file_name='signals.pkl',
-                  num_states=2, transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]])):
+                  num_states=2, transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]]),
+                  SNR=0):
 
-    data = {'rts': [], 'noisy_signal': []}
+    current_time  = time.time()
+    # data = {'rts': [], 'noisy_signal': []}
 
     # print a pretty "processing" sign
     if verbose:
@@ -20,6 +24,8 @@ def generate_data(num_data_points=10000, num_samples=1000,
                 print(line[:-1])
 
     verbose_name = "./data/" + file_name + "_" + "10000"
+
+    writer = tf.io.TFRecordWriter("./data/" + file_name)
 
     for i in range(num_data_points):
 
@@ -30,23 +36,31 @@ def generate_data(num_data_points=10000, num_samples=1000,
             num_samples=num_samples
         )
 
+        # find average signal power
+        sig_avg_watts = np.mean([rts_value**2 for rts_value in rts])
+
+        # use this to create a desired noise level
+        noise_avg_watts = sig_avg_watts / (10 ** (SNR / 10))
+
+        # Generate the noise based on the desired SNR
+       
         if vary_noise:
-            # noise = noise + generate_gaussian_noise(num_samples=num_samples, mean=0, std=np.random.uniform(0, 0.5))
-            noise = noise + generate_sinusoidal_signal(num_samples=num_samples, freq=np.random.uniform(0, 0.2), amplitude=np.random.uniform(0, 0.1), phase=np.random.uniform(0, 2*np.pi))
-            
+            noise = generate_gaussian_noise(num_samples=1000, mean=0, std=np.sqrt(noise_avg_watts) * np.random.uniform(0.01, 3))
+        else:
+            noise = generate_gaussian_noise(num_samples=1000, mean=0, std=np.sqrt(noise_avg_watts))
 
         # Add the noise to the RTS signal
         noisy_signal = rts + noise
 
         # add the noise and the rts to the data
-        data['rts'].append(np.reshape(rts, (num_samples)))
-        data['noisy_signal'].append(np.reshape(noisy_signal, (num_samples)))
+        # data['rts'].append()
+        # data['noisy_signal'].append()
 
-        if ((i + 1) % 10_000 == 0):
-            with open("./data/" + file_name + "_" + str(i + 1), 'wb') as f:
-                pickle.dump(data, f)
-            f.close()
-            verbose_name = "./data/" + file_name + "_" + str(i + 1)
+        # if ((i + 1) % 10_000 == 0):
+        #     with open("./data/" + file_name + "_" + str(i + 1), 'wb') as f:
+        #         pickle.dump(data, f)
+        #     f.close()
+        #     verbose_name = "./data/" + file_name + "_" + str(i + 1)
 
         # print a pretty percentage
         if verbose:
@@ -54,22 +68,39 @@ def generate_data(num_data_points=10000, num_samples=1000,
             sys.stdout.write("\r{:.2f}% completed, writing file: {}".format(percentage, verbose_name))
             sys.stdout.flush()
 
-    with open("./data/" + file_name + "_" + str(i + 1), 'wb') as f:
-        pickle.dump(data, f)
-    f.close()
-    print()
+        # write the data to a tfrecord file
+        example = tf.train.Example(
+            features=tf.train.Features(
+                feature={
+                    'rts': tf.train.Feature(float_list=tf.train.FloatList(value=np.reshape(rts, (num_samples)))),
+                    'noisy_signal': tf.train.Feature(float_list=tf.train.FloatList(value=np.reshape(noisy_signal, (num_samples))))
+                }
+            )
+        )
+        writer.write(example.SerializeToString())
+    
+    writer.close()
+    print("That took {} seconds".format(time.time() - current_time))
+
 
 
 if __name__ == '__main__':
+
+    # clear the data folder
+    for file in os.listdir("./data"):
+        os.remove("./data/" + file)
+        print("Removed file: {}".format(file))
+
     generate_data(
         num_data_points=10_000,
         num_samples=1000,
         # noise=np.zeros(1000),
-        noise=generate_gaussian_noise(num_samples=1000, mean=0, std=0.2) + 
-              generate_sinusoidal_signal(num_samples=1000, freq=0.02, amplitude=0.02),
-        # vary_noise=True,
+        # noise=generate_gaussian_noise(num_samples=1000, mean=0, std=0.2) + 
+            #   generate_sinusoidal_signal(num_samples=1000, freq=0.02, amplitude=0.02),
+        vary_noise=True,
         verbose=True,
-        file_name='signals',
+        file_name='signals.tfrecord',
         num_states=2,
-        transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]])
+        transition_probs=np.array([[0.99, 0.01], [0.01, 0.99]]),
+        SNR=20
     )
